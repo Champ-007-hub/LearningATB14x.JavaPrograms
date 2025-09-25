3316155741cbae0bcb8865e6060a7bda4086366b
Chaitanya <chaitanyarane63@gmail.com>
2025-09-25 12:28:18 +0530

    Automation programme Hello World

---
+++ src/Printf_table.java
@@ -0,0 +1,10 @@
+public class Printf_table {
+    public static void main(String[] args) {
+        int table =9;
+System.out.printf("%d*1= %d", table, table*1);
+System.out.println();
+        System.out.printf("%d*2= %d", table, table*2);
+        System.out.println();
+        System.out.printf("%d*3= %d", table, table*3);
+    }
+}
